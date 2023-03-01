270e430f71a84623fa01584dca896d7bdfe01701
Muyiwa Fawole <fawolemuyiwa@rocketmail.com>
2023-03-01 08:58:12 +0100

    Fix Rubocop and add seed

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -2,9 +2,9 @@ module Api
   class Api::GreetingsController < ApplicationController
     def index
       count_greetings = Greeting.count
-    random_offset = rand(count_greetings)
-    greetings = Greeting.offset(random_offset).first
-    render json: greetings
+      random_offset = rand(count_greetings)
+      greetings = Greeting.offset(random_offset).first
+      render json: greetings
     end
   end
 end
